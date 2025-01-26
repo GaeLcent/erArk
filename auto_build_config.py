e5b640a0aa31f8817e2d0ed226ae717a89a8d6c2
Gaelcent <1907131949@qq.com>
2025-01-26 17:40:02 +0800

    dev

--- auto_build_config.py
+++ auto_build_config.py
@@ -267,9 +267,9 @@ for i in file_list:
     now_file = os.path.join(config_dir, i)
     build_csv_config(now_file, i, 0, 0)
     index += 1
-import time
-start_time = time.time()
+
 talk_file_list = os.listdir(talk_dir)
+import time
 start_time = time.time()
 for i in talk_file_list:
     # 跳过ai文件夹
@@ -283,8 +283,8 @@ for i in talk_file_list:
         build_csv_config(now_f, f, 1, 0)
 end_time = time.time()
 total_time = end_time - start_time
-
 print(f"The for loop ran in {total_time:.4f} seconds.")
+
 target_file_list = os.listdir(target_dir)
 for i in target_file_list:
     now_dir = os.path.join(target_dir, i)
